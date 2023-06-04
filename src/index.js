504c07a6682fa62bdea3509ad7458d1068609523
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-06-05 07:40:57 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,13 @@
-import { legacy_createStore } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Provider } from 'react-redux'
+import App from './components/App'
+import store from './store'
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DEL_TODO = "DEL_TODO"; 
-
-const addToDo = text => {
-    return {
-        type: ADD_TODO,
-        text
-    };
-};
-
-const deleteToDo = id => {
-    return {
-        type: DEL_TODO,
-        id
-    }
-}
-const reducer = (state = [], action) => {
-    switch(action.type) {
-        case ADD_TODO:
-            return [...state, {text: action.text, id: Date.now()}];
-        case DEL_TODO:
-            return state.filter(todo => action.id !== todo.id);
-        default:
-            return state;
-    }
-};
-
-const store = legacy_createStore(reducer);
-const dispatchAddToDo = text => {
-    store.dispatch(addToDo(text))
-};
-const dispatchDeleteToDo = e => {
-    const id = parseInt(e.target.parentNode.id);
-    store.dispatch(deleteToDo(id))
-};
-
-const paintToDos = () => {
-    const toDos = store.getState();
-    ul.innerHTML = "";
-    toDos.forEach(toDo => {
-        const li = document.createElement("li");
-        const button = document.createElement("button");
-        button.innerText = "삭제";
-        button.addEventListener("click", dispatchDeleteToDo);
-        li.id = toDo.id;
-        li.innerText = toDo.text;
-        li.appendChild(button);
-        ul.appendChild(li);
-    })
-}
-
-store.subscribe(paintToDos)
-
-const onSubmit = e => {
-    e.preventDefault();
-    const toDo = input.value
-    input.value = "";
-    dispatchAddToDo(toDo)
-}
-
-form.addEventListener("submit", onSubmit)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
+);
